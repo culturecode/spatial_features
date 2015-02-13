b2500eed8e9541f813bde5920605c81b5e452148
Ryan Wallace <rywall@gmail.com>
2015-02-13 11:55:11 -0800

    Register geometry type as well to avoid warning.

--- config/initializers/register_oids.rb
+++ config/initializers/register_oids.rb
@@ -1,4 +1,5 @@
 ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.tap do |klass|
   # Use identity which does no casting
   klass::OID.register_type('geography', klass::OID::Identity.new)
+  klass::OID.register_type('geometry', klass::OID::Identity.new)
 end
