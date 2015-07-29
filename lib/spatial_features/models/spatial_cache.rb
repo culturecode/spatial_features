b2eece31fd77c5daf4cfdff5ef1377d27d79dd0e
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2015-07-28 20:04:15 -0700

    Add stale? method to spatial cache

--- lib/spatial_features/models/spatial_cache.rb
+++ lib/spatial_features/models/spatial_cache.rb
@@ -1,3 +1,7 @@
 class SpatialCache < ActiveRecord::Base
-  belongs_to :spatial_model, :polymorphic => true
+  belongs_to :spatial_model, :polymorphic => true, :inverse_of => :spatial_cache
+
+  def stale?
+    spatial_model.has_spatial_features_hash? && self.features_hash != spatial_model.features_hash
+  end
 end
