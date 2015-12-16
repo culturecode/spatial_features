f0c1888beb69a4a6f4bfe34ab1dfc79f7aacb13f
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2015-12-16 11:21:44 -0800

    Fixed intersection area of records with self-overlapping features
    Instead of taking the sum of all the intersection areas, the union of each record's features is first calculated, and then the overlapping areas of those two shapes is calculated. This prevents self overlapping features from being counted twice.

--- lib/spatial_features/has_spatial_features.rb
+++ lib/spatial_features/has_spatial_features.rb
@@ -39,7 +39,7 @@ module SpatialFeatures
         scope = joins_features_for(other).select("#{table_name}.*").group("#{table_name}.#{primary_key}")
         scope = scope.where('ST_DWithin(features_for.geom, features_for_other.geom, ?)', buffer_in_meters) if buffer_in_meters
         scope = scope.select("MIN(ST_Distance(features_for.geom, features_for_other.geom)) AS distance_in_meters") if options[:distance]
-        scope = scope.select("SUM(ST_Area(ST_Intersection(features_for.geom, features_for_other.geom))) AS intersection_area_in_square_meters") if options[:intersection_area]
+        scope = scope.select("ST_Area(ST_Intersection(ST_UNION(features_for.geom), ST_UNION(features_for_other.geom))) AS intersection_area_in_square_meters") if options[:intersection_area]
       end
 
       return scope
