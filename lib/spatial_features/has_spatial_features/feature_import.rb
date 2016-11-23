2075447f1c45580b7056cf6b8c6b3b6a5b69eb6a
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2016-11-23 13:56:36 -0800

    Add FeatureImport tests
    Ensuring multiple importers are supported.

--- lib/spatial_features/has_spatial_features/feature_import.rb
+++ lib/spatial_features/has_spatial_features/feature_import.rb
@@ -2,35 +2,43 @@ module SpatialFeatures
   module FeatureImport
     def update_features!(skip_invalid: false, **import_options)
       ActiveRecord::Base.transaction do
-        each_spatial_feature_import(import_options) do |import|
-          if has_spatial_features_hash? && import.cache_key != features_hash
-            replace_features(import, skip_invalid)
-            update_attributes(:features_hash => import.cache_key)
-          elsif !has_spatial_features_hash?
-            replace_features(import, skip_invalid) and return true
-          end
+        imports = spatial_feature_imports(import_options)
+        cache_key = imports.collect(&:cache_key).join(',').hash
+
+        if has_spatial_features_hash? && cache_key != features_hash
+          import_features(imports)
+          validate_features!(skip_invalid)
+          update_attributes(:features_hash => cache_key)
+        elsif !has_spatial_features_hash?
+          import_features(imports)
+          validate_features!(skip_invalid)
         end
       end
     end
 
     private
 
-    def each_spatial_feature_import(options)
-      spatial_features_options.fetch(:import, {}).each do |data_method, importer_name|
-        yield "SpatialFeatures::Importers::#{importer_name}".constantize.new(send(data_method), options)
+    def spatial_feature_imports(options = {})
+      spatial_features_options.fetch(:import, {}).collect do |data_method, importer_name|
+        "SpatialFeatures::Importers::#{importer_name}".constantize.new(send(data_method), options)
       end
     end
 
-    def replace_features(import, skip_invalid)
+    def import_features(imports)
       features.destroy_all
-      self.features = import.features
+      features << imports.flat_map(&:features)
+    end
 
-      errors = import.errors.dup
-      import.features.each do |feature|
-        errors << "Feature #{feature.name}: #{feature.errors.full_messages.to_sentence}" if feature.errors.present?
+    def validate_features!(skip_invalid = false)
+      invalid = features.select {|feature| feature.errors.present? }
+      features.destroy(invalid)
+
+      return if skip_invalid
+      errors = invalid.collect do |feature|
+        "Feature #{feature.name}: #{feature.errors.full_messages.to_sentence}"
       end
 
-      if errors.present? && !skip_invalid
+      if errors.present?
         raise UpdateError, "Error updating #{self.class} #{self.id}. #{errors.to_sentence}"
       end
     end
