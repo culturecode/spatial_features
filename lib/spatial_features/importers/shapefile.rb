4420f8d1bb9f9e08c5a8401c256ec10640aaeafe
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2016-11-25 10:43:50 -0800

    Import shapefiles using correct projection
    We shell out to `gdalsrsinfo`, passing it the shapefile so it can tell us what the proj4 string is because there doesn’t seem to be a way to directly output the SRID. We then feed the proj4 string into ST_Transform to convert the geometry to SRID 4326.

--- lib/spatial_features/importers/shapefile.rb
+++ lib/spatial_features/importers/shapefile.rb
@@ -4,7 +4,6 @@ require 'ostruct'
 module SpatialFeatures
   module Importers
     class Shapefile < Base
-
       def cache_key
         @cache_key ||= Digest::MD5.hexdigest(features.to_json)
       end
@@ -13,19 +12,21 @@ module SpatialFeatures
 
       def each_record(&block)
         file = Download.open(@data, unzip: '.shp')
+        proj4 = proj4_from_file(file)
         RGeo::Shapefile::Reader.open(file.path) do |records|
           records.each do |record|
-            yield OpenStruct.new data_from_wkt(record.geometry.as_text).merge(:metadata => record.attributes)
+            yield OpenStruct.new data_from_wkt(record.geometry.as_text, proj4).merge(:metadata => record.attributes)
           end
         end
       end
 
-      def geom_from_text(wkt)
-        ActiveRecord::Base.connection.select_value("SELECT ST_GeomFromText('#{wkt}')")
+      def proj4_from_file(file)
+        `gdalsrsinfo "#{file.path}" -o proj4`[/'(.+)'/,1] # Sanitize "'+proj=utm +zone=11 +datum=NAD83 +units=m +no_defs '\n"
+      end
 
-      def data_from_wkt(wkt)
+      def data_from_wkt(wkt, proj4)
         ActiveRecord::Base.connection.select_one <<-SQL
-          SELECT ST_GeomFromText('#{wkt}') AS geog, GeometryType(ST_GeomFromText('#{wkt}')) AS feature_type
+          SELECT ST_Transform(ST_GeomFromText('#{wkt}'), '#{proj4}', 4326) AS geog, GeometryType(ST_GeomFromText('#{wkt}')) AS feature_type
         SQL
       end
     end
