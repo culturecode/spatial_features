95fa276d2803de6bb87535ec8564b73ba87ceef3
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2016-11-29 17:07:51 -0800

    Allow per-record fusion tables
    Some models require their own fusion tables due to the number of features. A model can provide a table_name option or directly override the fusion_table_name method of a model to provide a per-record table name.

--- lib/spatial_features/has_fusion_table_features.rb
+++ lib/spatial_features/has_fusion_table_features.rb
@@ -7,6 +7,8 @@ module SpatialFeatures
 
         include InstanceMethods
         extend ClassMethods
+
+        delegate :update_fusion_table, :delete_fusion_table, :fusion_table_id_cache, :to => self
       end
     end
 
@@ -15,26 +17,36 @@ module SpatialFeatures
         sanitize_sql(["spatial_model_id IN (?)", pluck(:id)])
       end
 
-      def init_fusion_table
-        delete_fusion_table
-        update_fusion_table
+      def update_fusion_tables
+        fusion_table_groups do |fusion_table_id, records, group_features|
+          puts "Processing table #{fusion_table_id} - #{records.inspect}"
+          API.set_features(fusion_table_id, group_features, :colour => fusion_table_features_options[:colour])
+        end
       end
 
-      def update_fusion_table
-        API.set_features(fusion_table_id, features, :colour => fusion_table_features_options[:colour])
-      end
-
-      def delete_fusion_table
-        API.delete_table(fusion_table_id)
-        @fusion_table_id = nil
-      end
-
-      def fusion_table_id
-        @fusion_table_id ||= API.find_or_create_table(table_name)
+      def delete_fusion_tables
+        fusion_table_groups do |fusion_table_id, records, group_features|
+          API.delete_table(fusion_table_id)
+        end
+        fusion_table_id_cache.clear
       end
 
       def acts_like_fusion_table_features?
         true
+      end
+
+      def fusion_table_id_cache
+        @fusion_table_id_cache ||= Hash.new do |hash, table_name|
+          hash[table_name] = API.find_or_create_table(table_name)
+        end
+      end
+
+      private
+
+      def fusion_table_groups
+        all.group_by(&:fusion_table_id).each do |fusion_table_id, records|
+          yield fusion_table_id, records, features.where(:spatial_model_id => records)
+        end
       end
     end
 
@@ -44,7 +56,18 @@ module SpatialFeatures
       end
 
       def fusion_table_id
-        self.class.fusion_table_id
+        fusion_table_id_cache[fusion_table_name]
+      end
+
+      def fusion_table_name
+        case fusion_table_features_options[:table_name]
+        when Symbol
+          send(fusion_table_features_options[:table_name])
+        when String
+          fusion_table_features_options[:table_name]
+        else
+          self.class.table_name
+        end
       end
 
       def to_fusion_condition
