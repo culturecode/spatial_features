d0c2e54c1a07aa935a905a30e5be2dc443eb1a7f
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2019-12-08 14:34:49 -0800

    Fix PostGIS 3.x compatibility
    It appears as though PostGIS 2.x would happily ignore empty geometry in `ST_Collect`, or perhaps empty geometry would retain the SRID it was derived from. Whatever the cause, now when we `ST_CollectionExtract`, if there is no geometry, it returns an empty collection whose SRID is 0. This causes an explosion if any of the other extracts returns geometry, because `ST_Collect` doesn’t want mixed SRIDs. To work around this, we simply remove any empty sets from the array before collecting.
    
    Alternatively, we could just not extract, but in 2.x testing it was found to be faster to extract homogenous sets of geometry, union them, and then reassemble through `ST_Collect`. It should not impact performance to perform 3 removals on an array of 3 or fewer elements, so we go with this method instead.

--- app/models/aggregate_feature.rb
+++ app/models/aggregate_feature.rb
@@ -5,12 +5,26 @@ class AggregateFeature < AbstractFeature
 
   # Aggregate the features for the spatial model into a single feature
   def refresh
-    self.geog = ActiveRecord::Base.connection.select_value <<~SQL
-      SELECT ST_Collect(ARRAY[
+    feature_array_sql = <<~SQL
+      ARRAY[
         (#{features.select('ST_UNION(ST_CollectionExtract(geog::geometry, 1))').to_sql}),
         (#{features.select('ST_UNION(ST_CollectionExtract(geog::geometry, 2))').to_sql}),
         (#{features.select('ST_UNION(ST_CollectionExtract(geog::geometry, 3))').to_sql})
-      ])::geography
+      ]
+    SQL
+
+    # Remove empty features so ST_COLLECT doesn't choke. This seems to be a difference between PostGIS 2.x and 3.x
+    compacted_feature_array_sql = <<~SQL
+      array_remove(
+        array_remove(
+          array_remove(#{feature_array_sql},
+          ST_GeomFromText('Point EMPTY')),
+        ST_GeomFromText('Linestring EMPTY')),
+      ST_GeomFromText('Polygon EMPTY'))
+    SQL
+
+    self.geog = ActiveRecord::Base.connection.select_value <<~SQL
+      SELECT ST_Collect(#{compacted_feature_array_sql})::geography
     SQL
     self.save!
   end
