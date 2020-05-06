232017bb4810258cffea79e90a8b41d4d408bb18
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2020-05-06 13:50:02 -0700

    Fix bug where aggregate feature could not be empty
    If the a record’s features were empty the aggregate would be empty and have `nil` `geog`, causing it to fail validation. Instead, we ensure that if all the features are empty, we create an empty multipolygon for the aggregate feature.

--- app/models/aggregate_feature.rb
+++ app/models/aggregate_feature.rb
@@ -15,7 +15,7 @@ class AggregateFeature < AbstractFeature
 
     # Remove empty features so ST_COLLECT doesn't choke. This seems to be a difference between PostGIS 2.x and 3.x
     self.geog = ActiveRecord::Base.connection.select_value <<~SQL
-      SELECT ST_Collect(unnest)::geography
+      SELECT COALESCE(ST_Collect(unnest)::geography, ST_GeogFromText('MULTIPOLYGON EMPTY'))
       FROM (SELECT unnest(#{feature_array_sql})) AS features
       WHERE NOT ST_IsEmpty(unnest)
     SQL
