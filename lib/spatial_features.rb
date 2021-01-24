b66c5bd8cc3fd3a95a6502e1e846e07f173ec6e3
Ryan Wallace <ryan@culturecode.ca>
2021-01-23 16:16:02 -0800

    Remove Fusion Tables
    This service has been shut down by Google.

--- lib/spatial_features.rb
+++ lib/spatial_features.rb
@@ -3,9 +3,6 @@ require 'delayed_job_active_record'
 require 'rgeo/shapefile'
 require 'nokogiri'
 require 'zip'
-require 'googleauth'
-require 'google/apis/fusiontables_v2'
-require 'google/apis/drive_v3'
 
 # LIB
 require 'spatial_features/caching'
@@ -20,11 +17,6 @@ require 'spatial_features/has_spatial_features'
 require 'spatial_features/has_spatial_features/queued_spatial_processing'
 require 'spatial_features/has_spatial_features/feature_import'
 
-require 'spatial_features/has_fusion_table_features'
-require 'spatial_features/has_fusion_table_features/api'
-require 'spatial_features/has_fusion_table_features/configuration'
-require 'spatial_features/has_fusion_table_features/service'
-
 require 'spatial_features/importers/base'
 require 'spatial_features/importers/file'
 require 'spatial_features/importers/kml'
@@ -38,7 +30,6 @@ require 'spatial_features/engine'
 
 # Load the act method
 ActiveRecord::Base.send :extend, SpatialFeatures::ActMethod
-ActiveRecord::Base.send :extend, SpatialFeatures::FusionTables::ActMethod
 
 # Suppress date warnings when unzipping KMZ saved by Google Earth, see https://github.com/rubyzip/rubyzip/issues/112
 Zip.warn_invalid_date = false
