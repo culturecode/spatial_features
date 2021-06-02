244115496d8d5b7021edbd350a68a809daf287d4
Alex Dunae <alex@dunae.ca>
2021-06-02 15:05:08 -0700

    Update validation to match against basename of SHP file

--- spec/lib/spatial_features/validation.rb
+++ spec/lib/spatial_features/validation.rb
@@ -5,12 +5,12 @@ describe SpatialFeatures do
     let(:data) { shapefile_without_shape_format }
 
     it 'skips validation with allow_generic_zip_files option' do
-      expect { SpatialFeatures::Validation.validate_shapefile_archive!("file.zip", SpatialFeatures::Download.entries(data), allow_generic_zip_files: true) }.not_to raise_exception
+      expect { SpatialFeatures::Validation.validate_shapefile_archive!(SpatialFeatures::Download.entries(data), allow_generic_zip_files: true) }.not_to raise_exception
     end
 
     it 'performs validation without allow_generic_zip_files option' do
-      expect { SpatialFeatures::Validation.validate_shapefile_archive!("file.zip", SpatialFeatures::Download.entries(data), allow_generic_zip_files: false) }.to \
-        raise_exception(SpatialFeatures::Importers::IncompleteShapefileArchive, /file\.shp/)
+      expect { SpatialFeatures::Validation.validate_shapefile_archive!(SpatialFeatures::Download.entries(data), allow_generic_zip_files: false) }.to \
+        raise_exception(SpatialFeatures::Importers::IncompleteShapefileArchive, /FirstNationReserves\.shp/)
     end
   end
 end
