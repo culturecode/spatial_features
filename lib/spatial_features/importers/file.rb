36fd27b62da535f7f6457d487a4d9b3347bfb92f
Alex Dunae <alex@dunae.ca>
2021-08-19 18:34:04 -0700

    Whitespace and docs

--- lib/spatial_features/importers/file.rb
+++ lib/spatial_features/importers/file.rb
@@ -13,13 +13,12 @@ module SpatialFeatures
         raise ImportError, INVALID_ARCHIVE
       end
 
-
       # The File importer may be initialized multiple times by `::create_all` if it
-      # receives ZIP data containinng multiple KML and SHP files
+      # receives ZIP data containing multiple KML or SHP files. We use `current_file`
+      # to distinguish which file in the archive is currently being
+      # processed.
       #
-      # We use `current_file` to distinguish which file in the archive is currently being
-      # processed. If no `current_file` is passed then we just take the first valid
-      # file that we find.
+      # If no `current_file` is passed then we just take the first valid file that we find.
       def initialize(data, *args, current_file: nil, **options)
         begin
           current_file ||= Download.open_each(data, unzip: [/\.kml$/, /\.shp$/], downcase: true).first
@@ -31,8 +30,10 @@ module SpatialFeatures
         when '.kml'
           __setobj__(KMLFile.new(current_file, *args))
         when '.shp'
-          # We pass the raw `data` to Importers::Shapefile so it can re-examine any
-          # ZIP archives and see any related SHX, DBF and PRJ files
+          # We pass the raw `data` to Importers::Shapefile since it needs the entire
+          # ZIP archive to process a SHP file's related SHX, DBF and PRJ files.
+          # `shp_file_name` is also passed to distinguish between multiple SHP files in the
+          # same ZIP archive.
           __setobj__(
             Shapefile.new(data, *args, **options, shp_file_name: ::File.basename(current_file&.path))
           )
