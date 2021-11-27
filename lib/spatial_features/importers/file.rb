bda9f273b774d884c35a5c578dadfc0c11f5ebd6
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2021-11-26 21:04:35 -0800

    Allow File import to handle JSON files

--- lib/spatial_features/importers/file.rb
+++ lib/spatial_features/importers/file.rb
@@ -3,14 +3,16 @@ require 'open-uri'
 module SpatialFeatures
   module Importers
     class File < SimpleDelegator
-      INVALID_ARCHIVE = "Archive did not contain a .kml or .shp file. Supported formats are KMZ, KML, and zipped ArcGIS shapefiles.".freeze
+      INVALID_ARCHIVE = "Archive did not contain a .kml, .shp, .json, or .geojson file.".freeze
+      SUPPORTED_FORMATS = "Supported formats are KMZ, KML, zipped ArcGIS shapefiles, ESRI JSON, and GeoJSON.".freeze
 
+      FILE_PATTERNS = [/\.kml$/, /\.shp$/, /\.json$/, /\.geojson$/]
       def self.create_all(data, **options)
-        Download.open_each(data, unzip: [/\.kml$/, /\.shp$/], downcase: true).map do |file|
+        Download.open_each(data, unzip: FILE_PATTERNS, downcase: true).map do |file|
           new(data, **options, current_file: file)
         end
       rescue Unzip::PathNotFound
-        raise ImportError, INVALID_ARCHIVE
+        raise ImportError, INVALID_ARCHIVE + " " + SUPPORTED_FORMATS
       end
 
       # The File importer may be initialized multiple times by `::create_all` if it
@@ -21,7 +23,7 @@ module SpatialFeatures
       # If no `current_file` is passed then we just take the first valid file that we find.
       def initialize(data, *args, current_file: nil, **options)
         begin
-          current_file ||= Download.open_each(data, unzip: [/\.kml$/, /\.shp$/], downcase: true).first
+          current_file ||= Download.open_each(data, unzip: FILE_PATTERNS, downcase: true).first
         rescue Unzip::PathNotFound
           raise ImportError, INVALID_ARCHIVE
         end
@@ -31,8 +33,10 @@ module SpatialFeatures
           __setobj__(KMLFile.new(current_file, *args, **options))
         when '.shp'
           __setobj__(Shapefile.new(current_file, *args, **options))
+        when '.json', '.geojson'
+          __setobj__(OGR.new(current_file.path, *args, **options))
         else
-          raise ImportError, "Could not import file. Supported formats are KMZ, KML, and zipped ArcGIS shapefiles"
+          raise ImportError, "Could not import file. " + SUPPORTED_FORMATS
         end
       end
     end
