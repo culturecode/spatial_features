b9e4a75b1f7e99b41bda0b72f1ddd44dc22b1e3c
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2021-12-14 21:13:31 -0800

    Use same name for Feature bounds as on model

--- lib/spatial_features/has_spatial_features.rb
+++ lib/spatial_features/has_spatial_features.rb
@@ -203,7 +203,7 @@ module SpatialFeatures
 
     def bounds
       if association(:aggregate_feature).loaded?
-        aggregate_feature&.feature_bounds
+        aggregate_feature&.bounds
       else
         result = aggregate_features.pluck(:north, :east, :south, :west).first
         [:north, :east, :south, :west].zip(result.map {|bound| BigDecimal(bound) }).to_h.with_indifferent_access if result
