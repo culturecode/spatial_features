bc5b691a764685dd7e5090837bbfda2d15e79f97
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2021-12-20 00:49:16 -0800

    Add KML mime type
    We deal with KML, so we should include it in the mime types.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,2 +1,3 @@
 Mime::Type.register "application/vnd.geo+json", :geojson
 Mime::Type.register "application/vnd.mapbox-vector-tile", :mvt
+Mime::Type.register "application/vnd.google-earth.kml+xml", :kml
