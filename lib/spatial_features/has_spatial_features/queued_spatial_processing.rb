culturecode/spatial_features

commit 9dc4eb2b3675f7f7a38591d9054c69d4e4af9c08

Allow spatial processing status to be cached
This avoids n+1 queries when utilizing the processing status in lists.

lib/spatial_features/has_spatial_features/queued_spatial_processing.rb
@@ -11,7 +11,22 @@ module SpatialFeatures
     end
 
     def updating_features?
-      running_feature_update_jobs.exists?
+      case spatial_processing_status(:update_features!)
+      when :queued, :processing
+        true
+      else
+        false
+      end
+    end
+
+    def updating_features_failed?
+      spatial_processing_status(:update_features!) == :failure
+    end
+
+    def spatial_processing_status(method_name)
+      if has_attribute?(:spatial_processing_status_cache)
+        spatial_processing_status_cache[method_name.to_s]&.to_sym
+      end
     end
 
     def feature_update_error
@@ -27,17 +42,58 @@ module SpatialFeatures
     end
 
     def spatial_processing_jobs(suffix = nil)
-      Delayed::Job.where('queue LIKE ?', "#{spatial_processing_queue_name}#{suffix}%")
+      Delayed::Job.where(:queue => "#{spatial_processing_queue_name}#{suffix}")
     end
 
     private
 
     def queue_spatial_task(method_name, *args)
-      delay(:queue => spatial_processing_queue_name + method_name).send(method_name, *args)
+      Delayed::Job.enqueue SpatialProcessingJob.new(self, method_name, *args), :queue => spatial_processing_queue_name + method_name
     end
 
     def spatial_processing_queue_name
-      "#{self.class}/#{self.id}/"
+      "#{model_name}/#{id}/"
+    end
+
+    # CLASSES
+
+    class SpatialProcessingJob
+      def initialize(record, method_name, *args)
+        @record = record
+        @method_name = method_name
+        @args = args
+      end
+
+      def enqueue(job)
+        update_cached_status(:queued)
+      end
+
+      def perform
+        update_cached_status(:processing)
+        @record.send(@method_name, *@args)
+      end
+
+      def success(job)
+        update_cached_status(:success)
+      end
+
+      def error(job)
+        update_cached_status(:failure)
+      end
+
+      def failure(job)
+        update_cached_status(:failure)
+      end
+
+      private
+
+      def update_cached_status(state)
+        if @record.has_attribute?(:spatial_processing_status_cache)
+          cache = @record.spatial_processing_status_cache || {}
+          cache[@method_name] = state
+          @record.update_column(:spatial_processing_status_cache, cache)
+        end
+      end
     end
   end
 end

commit ad474582f35958bdb69247f1a9f5cb06c5bdf181

Fix test failure
Introduced in 9dc4eb2b3675f7f7a38591d9054c69d4e4af9c08. I was playing around with ways to make it possible to eager load the queued spatial processing jobs, but instead opted to cache their status on the spatial model. However, I unintentionally left one of the changes I made which was to no longer support wildcard queued job lookup.

lib/spatial_features/has_spatial_features/queued_spatial_processing.rb
@@ -41,8 +41,8 @@ module SpatialFeatures
       spatial_processing_jobs('update_features!').where.not(failed_at: nil)
     end
 
-    def spatial_processing_jobs(suffix = nil)
-      Delayed::Job.where(:queue => "#{spatial_processing_queue_name}#{suffix}")
+    def spatial_processing_jobs(method_name = nil)
+      Delayed::Job.where('queue LIKE ?', "#{spatial_processing_queue_name}#{method_name}%")
     end
 
     private