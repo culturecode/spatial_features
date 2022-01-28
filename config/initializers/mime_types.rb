996a8918720822749797f6d8ab7367b05c27b3e0
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2022-01-27 22:50:14 -0800

    Add missing kmz mime type

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,3 +1,4 @@
 Mime::Type.register "application/vnd.geo+json", :geojson
 Mime::Type.register "application/vnd.mapbox-vector-tile", :mvt
 Mime::Type.register "application/vnd.google-earth.kml+xml", :kml
+Mime::Type.register "application/vnd.google-earth.kmz", :kmz
