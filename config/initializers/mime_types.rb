37477777a2eeea7b41282bb99c4308062f3e0232
Ryan Wallace <ryan@culturecode.ca>
2022-02-03 15:54:42 -0800

    Add bounds mime type.
    We have added JSON endpoints that are solely for serving bounds data. Using a custom mime type for this instead allows us to use the basic JSON endpoint for an API.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,4 +1,5 @@
 Mime::Type.register "application/vnd.geo+json", :geojson
+Mime::Type.register "application/vnd.bounds+json", :bounds
 Mime::Type.register "application/vnd.mapbox-vector-tile", :mvt
 Mime::Type.register "application/vnd.google-earth.kml+xml", :kml
 Mime::Type.register "application/vnd.google-earth.kmz", :kmz
