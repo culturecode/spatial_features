5adf6d264291016f08a21a4f812b31b7b30dbe45
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2023-08-10 03:44:25 -0700

    Ensure deterministic SpatialProximity model a/b
    When querying for spatial proximities we generally are comparing members of one table against members of another table. However, we don't know which SpatialProximity column the models will end up in, either `model_a` or `model_b`, which means we have to look for both cases.
    
    Instead, we can ensure we know which column each will be stored in by performing a sort at insert time. We can make model a the record with the earliest model name and id (alphanumerically), and therefore determine which columns to query without having to also check for the case where the records were stored in the opposite columns.
    
    The only case that is unaffected is when the comparison models are the same. Though we could also look at the id columns, we're often not specifying a single id, therefore requiring complex SQL in the query instead of a single comparison in Ruby before constructing the query, so we just leave that case unoptimized.
    
    SpatialProximity.normalize can be used to 'flip' the model of rows where the records where not written to the alphanumerically 'correct' column.

--- app/models/spatial_proximity.rb
+++ app/models/spatial_proximity.rb
@@ -2,10 +2,34 @@ class SpatialProximity < ActiveRecord::Base
   belongs_to :model_a, :polymorphic => true
   belongs_to :model_b, :polymorphic => true
 
-  def self.between(scope_1, scope_2)
-    where <<-SQL.squish
-      (#{SpatialFeatures::Utils.polymorphic_condition(scope_1, 'model_a')} AND #{SpatialFeatures::Utils.polymorphic_condition(scope_2, 'model_b')}) OR
-      (#{SpatialFeatures::Utils.polymorphic_condition(scope_2, 'model_a')} AND #{SpatialFeatures::Utils.polymorphic_condition(scope_1, 'model_b')})
+  def self.between(scope1, scope2)
+    where condition_sql(scope1, scope2, <<~SQL.squish)
+      (#{SpatialFeatures::Utils.polymorphic_condition(scope1, 'model_a')} AND #{SpatialFeatures::Utils.polymorphic_condition(scope2, 'model_b')})
     SQL
+  end
+
+  def self.condition_sql(scope1, scope2, template, pattern_a = 'model_a', pattern_b = 'model_b')
+    scope1_type = SpatialFeatures::Utils.base_class_of(scope1).to_s
+    scope2_type = SpatialFeatures::Utils.base_class_of(scope2).to_s
+
+    if scope1_type < scope2_type
+      template
+    elsif scope1_type > scope2_type
+      template.gsub(pattern_a, 'model_c').gsub(pattern_b, pattern_a).gsub('model_c', pattern_b)
+    else
+      <<~SQL.squish
+        (#{template}) OR (#{template.gsub(pattern_a, 'model_c').gsub(pattern_b, pattern_a).gsub('model_c', pattern_b)})
+      SQL
+    end
+  end
+
+  # Ensure the 'earliest' model is always model a
+  def self.normalize
+    unnormalized
+      .update_all('model_a_type = model_b_type, model_b_type = model_a_type, model_a_id = model_b_id, model_b_id = model_a_id')
+  end
+
+  def self.unnormalized
+    where('model_a_type > model_b_type OR (model_a_type = model_b_type AND model_a_id > model_b_id)')
   end
 end
