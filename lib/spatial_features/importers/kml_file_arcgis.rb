c66d070e65fd16f921708165ec21e5c8d2a3d06d
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2024-02-07 22:37:45 -0800

    Fix bug that broke ArcGIS metadata import
    We're now importing the metadata from KML description node tables as part of the base KML importer, so no need to have special handling in the KML ArcGIS importer. Doing so causes metadata not to be imported.

--- lib/spatial_features/importers/kml_file_arcgis.rb
+++ lib/spatial_features/importers/kml_file_arcgis.rb
@@ -12,16 +12,6 @@ module SpatialFeatures
       rescue OpenURI::HTTPError
         raise ImportError, "ArcGIS Map Service not found. Ensure ArcGIS Server is running and accessible at #{path_or_url}."
       end
-
-      private
-
-      # ArcGIS includes metadata as an html table in the description
-      def each_record(&block)
-        super do |record|
-          record.metadata = Hash[Nokogiri::XML(record.metadata[:description]).css('td').collect(&:text).each_slice(2).to_a]
-          yield record
-        end
-      end
     end
   end
 end
