99108de19985d542db989cb3cecf5dfefbd3a925
Shup04 <bradleyschmidt04@gmail.com>
2026-09-01 14:01:39 -0700

    Handle photos with incomplete EXIF data

--- lib/spatial_features/importers/exif_photo.rb
+++ lib/spatial_features/importers/exif_photo.rb
@@ -4,22 +4,40 @@ require 'ostruct'
 module SpatialFeatures
   module Importers
     class ExifPhoto < Base
+      def initialize(data, **options)
+        options[:source_identifier] ||= ::File.basename(data.to_s)
+        super(data, **options)
+      end
+
       private
 
       def each_record
         photo = EXIFR::JPEG.new(@data)
         gps = photo.gps
+        unless usable_gps?(gps)
+          @warnings << 'No usable GPS coordinates were found in this photo.'
+          return
+        end
 
         yield OpenStruct.new(
           name: ::File.basename(@data),
           geog: "POINT(#{gps.longitude} #{gps.latitude})",
-          metadata: {
-            'capture_time' => photo.date_time_original.strftime('%Y-%m-%d %H:%M:%S'),
-            'altitude' => gps.altitude.to_s,
-            'camera_model' => photo.model
-          },
+          metadata: metadata_from(photo, gps),
           importable_image_paths: [@data]
         )
+      end
+
+      def usable_gps?(gps)
+        gps && gps.latitude.is_a?(Numeric) && gps.longitude.is_a?(Numeric) &&
+          (-90..90).cover?(gps.latitude) && (-180..180).cover?(gps.longitude)
+      end
+
+      def metadata_from(photo, gps)
+        {
+          'capture_time' => photo.date_time_original&.strftime('%Y-%m-%d %H:%M:%S'),
+          'altitude' => gps.altitude&.to_s,
+          'camera_model' => photo.model.presence
+        }.compact
       end
     end
   end
--- spec/lib/spatial_features/importers/exif_photo_spec.rb
+++ spec/lib/spatial_features/importers/exif_photo_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe SpatialFeatures::Importers::ExifPhoto do
   let(:photo_path) { fixture_file_path('bc25_bt_0030.JPG') }
 
-  subject(:features) do
-    described_class.new(photo_path).features
-  end
+  subject(:importer) { described_class.new(photo_path) }
+
+  let(:features) { importer.features }
 
   describe '#features' do
     it 'imports one feature from a geotagged photo' do
@@ -19,15 +19,48 @@ describe SpatialFeatures::Importers::ExifPhoto do
     end
 
     it 'includes EXIF metadata' do
-    expect(features.first.metadata).to eq(
-      'capture_time' => '2025-08-08 16:48:16',
-      'altitude' => '109.4',
-      'camera_model' => 'NIKON D7500'
-    )
+      expect(features.first.metadata).to eq(
+        'capture_time' => '2025-08-08 16:48:16',
+        'altitude' => '109.4',
+        'camera_model' => 'NIKON D7500'
+      )
     end
 
     it 'makes the photo available for attachment importing' do
       expect(features.first.importable_image_paths).to eq([photo_path])
+    end
+  end
+
+  context 'when the photo has no GPS coordinates' do
+    before do
+      allow(EXIFR::JPEG).to receive(:new).with(photo_path).and_return(double(gps: nil))
+    end
+
+    it 'does not import a feature' do
+      expect(features).to be_empty
+    end
+
+    it 'records a warning that identifies the problem' do
+      features
+
+      expect(importer.warnings).to include(a_string_matching(/GPS coordinates/i))
+    end
+
+    it 'identifies the source photo by filename' do
+      expect(importer.source_identifier).to eq('bc25_bt_0030.JPG')
+    end
+  end
+
+  context 'when optional EXIF metadata is absent' do
+    let(:gps) { double(latitude: 50.36145, longitude: -125.12249, altitude: nil) }
+    let(:photo) { double(gps: gps, date_time_original: nil, model: nil) }
+
+    before do
+      allow(EXIFR::JPEG).to receive(:new).with(photo_path).and_return(photo)
+    end
+
+    it 'imports the point without blank metadata values' do
+      expect(features.first.metadata).to eq({})
     end
   end
 end
